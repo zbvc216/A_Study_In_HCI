724ee4daf70a01424ae41a94b75311446245b9a9
James <zbvc216@live..rhul.ac.uk>
2018-02-04 15:53:56 +0000

    Started App
    Encountering a lot of issues with unfamiliar aspects of android studio
    which is slowing progress a lot.

---
+++ Diary App/app/src/main/java/com/james/green/journal/MainActivity.java
@@ -0,0 +1,30 @@
+package com.james.green.journal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        Thread load = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        load.start();
+    }
+}
